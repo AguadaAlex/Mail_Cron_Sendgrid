2697b8bc84d42e9d94a5fd5a9633addbdac59345
aaguada <alex.aguada@snoopconsulting.com>
2025-02-14 09:46:21 -0300

    Inicio

---
+++ src/main/java/com/EmailCron/EmailCron/EmailCronApplication.java
@@ -0,0 +1,13 @@
+package com.EmailCron.EmailCron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailCronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailCronApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EmailCron/EmailCron/EmailCronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmailCron.EmailCron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailCronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
